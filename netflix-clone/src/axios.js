88f49a706dc2cd5b7749e338479643221d2044b5
Zunair Khawaja <102663927+khzunair@users.noreply.github.com>
2022-09-23 21:56:29 +0500

    created basic structure of App
    Basic Structure of app is created with a row component and some axios and request files are created for later use

---
+++ netflix-clone/src/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+    // base url to make requests to the movie database
+    const instance = axios.create({
+        baseURL:"http://api.themoviedb.org/3",
+    });
+
+    instance.get('foo-bar');
+    // http://api.themoviedb.org/3/foo-bar
+
+export default instance;
---
+++ netflix-clone/src/request.js
@@ -0,0 +1,8 @@
+const APIKEY = "f98272b643248b7ed99788f250d93e8c" ;
+
+const requests ={
+    fetchTrending : `/trending/all/week?api_key=${APIKEY}`
+
+}
+
+export default requests
